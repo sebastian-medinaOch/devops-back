c3883c3ed758eb36d897f4aec5de365502e7c214
Sebastian Medina Ochoa <semedin@bancolombia.com.co>
2022-11-06 16:48:34 -0500

    Creación del useCase PersonUseCase

---
+++ persona/src/main/java/com/smo/persona/domain/usecase/PersonUseCase.java
@@ -0,0 +1,54 @@
+package com.smo.persona.domain.usecase;
+
+import com.smo.persona.domain.model.Person;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+
+@RequiredArgsConstructor
+public class PersonUseCase {
+
+    public HttpStatus createPerson(){
+        return HttpStatus.OK;
+    }
+
+    public Person getPerson(String clientNumDoc){
+        return Person.builder()
+                .clientName("Sebastian")
+                .clientLastName("Medina")
+                .clientYear("21")
+                .clientCity("Cali")
+                .clientTypeDoc("cc")
+                .clientNumDoc("100221")
+                .build();
+    }
+
+    public ArrayList<Person> getPersons(){
+        ArrayList<Person> persons = new ArrayList<>();
+        Person person1 = Person.builder()
+                .clientName("Sebastian")
+                .clientLastName("Medina")
+                .clientYear("21")
+                .clientCity("Cali")
+                .clientTypeDoc("cc")
+                .clientNumDoc("100221")
+                .build();
+        Person person2 = Person.builder()
+                .clientName("Sebastian2")
+                .clientLastName("Medina2")
+                .clientYear("23")
+                .clientCity("Calic")
+                .clientTypeDoc("cc")
+                .clientNumDoc("100221")
+                .build();
+        persons.add(person1);
+        persons.add(person2);
+        return  persons;
+    }
+
+    public HttpStatus deletePerson(String clientNumDoc){
+         return  HttpStatus.OK;
+    }
+
+}
