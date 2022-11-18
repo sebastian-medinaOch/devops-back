5ad941b78126fbb5e8e90916b18dcd237b168d4f
Sebastian Medina Ochoa <semedin@bancolombia.com.co>
2022-11-18 10:01:19 -0500

    Modificación de la función delete

--- persona/src/main/java/com/smo/persona/application/services/ServiceDeletePerson.java
+++ persona/src/main/java/com/smo/persona/application/services/ServiceDeletePerson.java
@@ -3,7 +3,6 @@ package com.smo.persona.application.services;
 import com.smo.persona.application.gateways.DeletePersonInt;
 import com.smo.persona.domain.usecase.PersonUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 
@@ -12,7 +11,7 @@ public class ServiceDeletePerson implements DeletePersonInt {
     private final PersonUseCase personUseCase;
 
     @Override
-    public HttpStatus deletePerson(String clientNumDoc){
-        return personUseCase.deletePerson(clientNumDoc);
+    public void deletePerson(String clientNumDoc){
+       personUseCase.deletePerson(clientNumDoc);
     }
 }
--- persona/src/main/java/com/smo/persona/infrastructure/rest/controller/EndpointDeletePersonByNumDoc.java
+++ persona/src/main/java/com/smo/persona/infrastructure/rest/controller/EndpointDeletePersonByNumDoc.java
@@ -1,11 +1,15 @@
 package com.smo.persona.infrastructure.rest.controller;
 
 import com.smo.persona.application.services.ServiceDeletePerson;
+import com.smo.persona.domain.answer.AnswerData;
+import com.smo.persona.domain.answer.DataResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/person")
@@ -15,8 +19,12 @@ public class EndpointDeletePersonByNumDoc {
     private final ServiceDeletePerson personService;
 
     @DeleteMapping("/deletePerson")
-    public HttpStatus deletePerson(@NotNull @RequestParam("clientNumDoc") String clientNumDoc){
-        return personService.deletePerson(clientNumDoc);
+    public ResponseEntity<AnswerData> deletePerson(@NotNull @RequestParam("clientNumDoc") String clientNumDoc){
+        personService.deletePerson(clientNumDoc);
+        DataResponse dataResponse = DataResponse.builder().message("Sastifactorio").data(Optional.of("El cliente que " +
+                "se eliminó fue con el numero de documento: " +  clientNumDoc)).build();
+        AnswerData answerData = new AnswerData(HttpStatus.OK, dataResponse);
+        return new ResponseEntity<>(answerData, answerData.getStatus());
     }
 
 }
