76fd281bf49c018a7d1b0aa1cb70b599b1513eec
Sebastian Medina Ochoa <semedin@bancolombia.com.co>
2022-11-29 10:31:41 -0500

    Validaciones del deletePerson

--- person/src/main/java/com/smo/person/application/services/ServiceDeletePerson.java
+++ person/src/main/java/com/smo/person/application/services/ServiceDeletePerson.java
@@ -1,6 +1,7 @@
 package com.smo.person.application.services;
 
 import com.smo.person.application.gateways.DeletePersonInt;
+import com.smo.person.application.request.PersonRequest;
 import com.smo.person.domain.exception.BussinessException;
 import com.smo.person.domain.usecase.PersonUseCase;
 import com.smo.person.infrastructure.persistencia.entity.PersonEntity;
@@ -16,8 +17,17 @@ public class ServiceDeletePerson implements DeletePersonInt {
     @Override
     public void deletePerson(String clientNumDoc) throws BussinessException {
         validateRequest(clientNumDoc);
-        PersonEntity personEntity = personUseCase.getPersonByNumDoc(clientNumDoc);
+        PersonEntity personEntity = validatePerson(clientNumDoc);
         personUseCase.deletePerson(personEntity.getPersonId());
+    }
+
+    private PersonEntity validatePerson(String clienNumDoc) throws BussinessException {
+        PersonEntity personRepository = personUseCase.getPersonByNumDoc(clienNumDoc);
+        if (personRepository == null) {
+            throw new BussinessException(HttpStatus.NOT_FOUND, "La persona que quieres eliminar, no existe " +
+                    "en la base de datos");
+        }
+        return personRepository;
     }
 
     private void validateRequest(String clientNumDoc) throws BussinessException {
